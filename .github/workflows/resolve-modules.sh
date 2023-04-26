de17eb332a3b918b19eea7b7df3139183686a4c8
Lovro Mažgon <lovro.mazgon@gmail.com>
2023-04-26 16:24:38 +0200

    find root module as well

--- .github/workflows/resolve-modules.sh
+++ .github/workflows/resolve-modules.sh
@@ -6,5 +6,5 @@
 
 echo "Resolving modules in $(pwd)"
 
-PATHS=$(find . -mindepth 2 -type f -name go.mod -printf '{"workdir":"%h"},')
+PATHS=$(find . -type f -name go.mod -printf '{"workdir":"%h"},')
 echo "::set-output name=matrix::{\"include\":[${PATHS%?}]}"
